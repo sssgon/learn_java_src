32bf863e5f871c4fe436bc695a6ae08e14d370e7
sssgon <sssgon@163.com>
2018-02-27 16:02:35 +0800

    接口default方法测试

--- src/com/yusys/annotation/AtInterface.java
+++ src/com/yusys/annotation/AtInterface.java
@@ -1,5 +1,13 @@
 package com.yusys.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AtInterface {
-	
+	public String id();
+	public String describtion() default "default";
 }
